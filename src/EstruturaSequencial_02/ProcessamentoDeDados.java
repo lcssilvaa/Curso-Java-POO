c0cf46c2de6ee12ed07be50d252adafb15f730ae
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-08-02 17:29:14 -0300

    "Seção 4: Aula 25"

---
+++ src/EstruturaSequencial_02/ProcessamentoDeDados.java
@@ -0,0 +1,57 @@
+package EstruturaSequencial_02;
+
+public class ProcessamentoDeDados {
+
+    public static void main(String[] args) {
+
+        /* Exercício 1
+
+        int x, y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        =====================================================
+
+        Exercício 2
+
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+        System.out.println(area);
+
+        ====================================================
+
+        Exercício 3
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b; -> casting
+
+        System.out.println(resultado);
+
+        ====================================================
+
+        Exercício 4 */
+
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a; // casting
+
+        System.out.println(b);
+
+    }
+}
